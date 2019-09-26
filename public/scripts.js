5ee98748842719290bd8752c9a3bd4d367f42672
Junior <gilmar_junior@hotmaill.com.br>
2019-09-25 21:21:33 -0300

    create lightbox when image clicked

--- public/scripts.js
+++ public/scripts.js
@@ -5,13 +5,20 @@ let createImage = document.createElement("img");
 $lzyAllImages.forEach((e)=>{    
     e.addEventListener("click", ()=>{
         document.querySelector("body").insertAdjacentHTML("beforeend", `
-            <div class="lzy-lightbox-display">
-                <span id="lzyCloseLightbox">x</span>
+            <div id="lzyLightboxDisplay" class="lzy-lightbox-display on">
                 <div class="lzy-modal">
+                    <span id="lzyCloseLightbox">&#215;</span>
                     <img src="${e.src}" />
                 </div>
             </div>
         `);
+
+        let $closeButton = document.getElementById("lzyCloseLightbox");
+        let $lightboxWindow = document.getElementById("lzyLightboxDisplay");
+
+        $closeButton.click() = ()=>{
+            $lightboxWindow.classList.remove("on");
+        }
     });
 });
 
